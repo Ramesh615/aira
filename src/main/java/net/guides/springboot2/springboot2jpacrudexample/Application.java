aa543de94d496c7be0e92522fcb466930eac82df
RAMESH AITHAGOINA <88446859+Ramesh615@users.noreply.github.com>
2022-06-23 04:16:35 +0530

    Update Application.java

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -2,7 +2,7 @@ package net.guides.springboot2.springboot2jpacrudexample;
 
 import org.springframework.boot.SpringApplication;
 
-import org.springframework.boot.SpringApplicationBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
