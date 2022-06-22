914f323c4852679e1a9f72ec315d66089ca0d7cb
RAMESH AITHAGOINA <88446859+Ramesh615@users.noreply.github.com>
2022-06-23 04:12:38 +0530

    Update Application.java

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -13,4 +13,8 @@ public class Application {
 	public static void main(String[] args) { 
 		SpringApplication.run(Application.class, args); 
 	}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(Application.class);
+  }
 }
