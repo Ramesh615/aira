95c74abb4ec2be7db799fdd31269eff4134e730b
RAMESH AITHAGOINA <88446859+Ramesh615@users.noreply.github.com>
2022-06-23 04:22:40 +0530

    Update Application.java

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -1,7 +1,7 @@
 package net.guides.springboot2.springboot2jpacrudexample;
 
 import org.springframework.boot.SpringApplication;
- import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
